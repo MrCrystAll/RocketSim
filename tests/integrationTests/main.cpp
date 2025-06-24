fb8f3ca793298d523425ffab7016fd62c41cf30f
Mathieu Suchet <msuchet63@gmail.com>
2025-06-24 15:19:46 +0200

    Trying integration tests

---
+++ tests/integrationTests/main.cpp
@@ -0,0 +1,18 @@
+#include <RocketSim/version.h>
+#include <RocketSim/RocketSim.h>
+
+#include <RocketSim/Sim/Arena/Arena.h>	
+
+#include <iostream>
+
+int main() {
+	using std::cout, std::endl;
+	using namespace RocketSim;
+
+	Init("./resources/collision_meshes", true);
+
+	Arena* arena = Arena::Create(GameMode::SOCCAR);
+	arena->Step(100);
+
+	cout << "Successfully integrated RocketSim " << ROCKETSIM_VERSION << "!" << endl;
+}
